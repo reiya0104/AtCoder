16ff247537846bbcad5e3f8044024528a4b9dd2d
Reiya <reiya0104@ezweb.ne.jp>
2021-02-27 16:04:28 +0900

    Ex19

--- Python/translate_filename.py
+++ Python/translate_filename.py
@@ -5,5 +5,9 @@ S = input("翻訳する文を入力してください: ")
 
 translator = Translator()
 trans_en = translator.translate(S)
+trans_jp = translator.translate(trans_en.text, dest="ja")
 trans_filename = trans_en.text.replace(" ", "_")
-print(trans_filename)
+print(f"英訳: {trans_en.text}")
+print(f"再翻訳: {trans_jp.text}")
+print(f"英訳ファイル名: {trans_filename}")
+
