408d178d64e0778da1e01778db264645643ca112
Reiya <reiya0104@ezweb.ne.jp>
2021-02-27 15:14:00 +0900

    delete comment

--- Python/translate_filename.py
+++ Python/translate_filename.py
@@ -5,6 +5,5 @@ S = input("翻訳する文を入力してください: ")
 
 translator = Translator()
 trans_en = translator.translate(S)
-# print(trans_en.text)
 trans_filename = trans_en.text.replace(" ", "_")
 print(trans_filename)
