5939477cc2cead3e46ebfbf6ff5ec981a6abc170
Reiya <reiya0104@ezweb.ne.jp>
2021-02-27 15:11:40 +0900

    Ex17 and Make translate file

---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(trans_en.text)
+# for v in trans_en.text:
+#     print(ord(v))
--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,10 @@
-print("Hello, world!")
+# -*- coding: utf-8 -*-
+from googletrans import Translator
+
+S = input("翻訳する文を入力してください: ")
+
+translator = Translator()
+trans_en = translator.translate(S)
+# print(trans_en.text)
+trans_filename = trans_en.text.replace(" ", "_")
+print(trans_filename)
